6056cb847e5f0927973d79b45bab4c90dae99499
Prulucas <plbrandsilva@gmail.com>
2025-03-14 21:42:04 -0300

    Aula 52 - Capturando os valores dos inputs com React Hook Form

--- spa-breakingnews/src/components/input/input.jsx
+++ spa-breakingnews/src/components/input/input.jsx
@@ -1,7 +1,7 @@
 import {InputSpace } from "./inputStyled"
 
-export function Input ({type, placeholder, name}) {
+export function Input ({type, placeholder, name, register}) {
     return (
-        <InputSpace type={type} placeholder={placeholder} name={name} />
+        <InputSpace type={type} placeholder={placeholder} {...register(name)} />
     )
 }
--- spa-breakingnews/src/pages/authentication/authentication.jsx
+++ spa-breakingnews/src/pages/authentication/authentication.jsx
@@ -1,27 +1,43 @@
 import { Input } from "../../components/input/input";
 import { AuthContainer, Section } from "./authenticationStyled";
-import {Button} from "../../components/button/button.jsx"
+import {Button} from "../../components/button/button.jsx";
+import {useForm} from "react-hook-form";
+import z from "zod";
+import {zodResolver} from "@hookform/resolvers/zod"
 
 
 export function Authentication() {
+
+    const {register: registerSignup, handleSubmit: handleSubmitSignup, formState: {errors: errorsSignup}} = useForm();
+
+    const {register: registerSignin, handleSubmit: handleSubmitSignin, formState: {errors: errorsSignin}} = useForm();
+
+    function inHandleSubmit(data){
+        console.log(data)
+    }
+
+    function upHandleSubmit(data){
+        console.log(data)
+    }
+
     return (
         <AuthContainer>
             <Section type="signin">
                 <h2>Entrar</h2>
-                <form>
-                    <Input type="email" placeholder="Email" name="email" />
-                    <Input type="password" placeholder="Senha" name="password" />
+                <form onSubmit={handleSubmitSignin(inHandleSubmit)}>
+                    <Input type="email" placeholder="Email" name="email" register={registerSignin} />
+                    <Input type="password" placeholder="Senha" name="password" register={registerSignin} />
                     <Button type="submit" text="Entrar" />
                 </form>
             </Section>
 
             <Section type="signup">
                 <h2>Cadastrar</h2>
-                <form>
-                    <Input type="text" placeholder="Nome" name="name" />
-                    <Input type="email" placeholder="Email" name="email" />
-                    <Input type="password" placeholder="Senha" name="password" />
-                    <Input type="password" placeholder="Confirmar senha" name="password" />
+                <form onSubmit={handleSubmitSignup(upHandleSubmit)}>
+                    <Input type="text" placeholder="Nome" name="name" register={registerSignup} />
+                    <Input type="email" placeholder="Email" name="email" register={registerSignup} />
+                    <Input type="password" placeholder="Senha" name="password" register={registerSignup} />
+                    <Input type="password" placeholder="Confirmar senha" name="confirmPassword" register={registerSignup} />
                     <Button type="submit" text="Cadastrar" />
                 </form>
             </Section>
