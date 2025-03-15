bc34c56aee454b85f02859fe7fbb4e22c230d3dd
Prulucas <plbrandsilva@gmail.com>
2025-03-15 20:16:07 -0300

    Aula 53 - Validando os campos do formulário com Zod

--- spa-breakingnews/src/components/navbar/navbar.jsx
+++ spa-breakingnews/src/components/navbar/navbar.jsx
@@ -2,13 +2,11 @@ import { Link, Outlet, useNavigate } from "react-router-dom";
 import logo from "../../images/LogoBN.png";
 import {Nav, ImgLogo, InputSpace, ErrorSpan} from "./navbarStyled.jsx"
 import {useForm} from "react-hook-form"
-import z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod"
 import {Button} from "../button/button.jsx"
+import { searchSchema } from "../../schemas/searchSchema.js";
 
-const searchSchema = z.object({
-    title: z.string().nonempty({message: "A pesquisa não pode ser vazia"}).refine(value => !/^\s*$/.test(value), {message: "A pesquisa não pode ter apenas espaços"}),
-})
+
 
 export function Navbar(){
     const {register, handleSubmit, reset, formState: {errors}} = useForm({
--- spa-breakingnews/src/pages/authentication/authentication.jsx
+++ spa-breakingnews/src/pages/authentication/authentication.jsx
@@ -2,15 +2,18 @@ import { Input } from "../../components/input/input";
 import { AuthContainer, Section } from "./authenticationStyled";
 import {Button} from "../../components/button/button.jsx";
 import {useForm} from "react-hook-form";
-import z from "zod";
 import {zodResolver} from "@hookform/resolvers/zod"
+import { signinSchema } from "../../schemas/signinSchema.js";
+
+import {ErrorSpan} from "../../components/navbar/navbarStyled.jsx"
+import { signupSchema } from "../../schemas/signupSchema.js";
 
 
 export function Authentication() {
 
-    const {register: registerSignup, handleSubmit: handleSubmitSignup, formState: {errors: errorsSignup}} = useForm();
+    const {register: registerSignup, handleSubmit: handleSubmitSignup, formState: {errors: errorsSignup}} = useForm({resolver: zodResolver(signupSchema)});
 
-    const {register: registerSignin, handleSubmit: handleSubmitSignin, formState: {errors: errorsSignin}} = useForm();
+    const {register: registerSignin, handleSubmit: handleSubmitSignin, formState: {errors: errorsSignin}} = useForm({resolver: zodResolver(signinSchema)});
 
     function inHandleSubmit(data){
         console.log(data)
@@ -26,18 +29,26 @@ export function Authentication() {
                 <h2>Entrar</h2>
                 <form onSubmit={handleSubmitSignin(inHandleSubmit)}>
                     <Input type="email" placeholder="Email" name="email" register={registerSignin} />
+                    {errorsSignin.email && <ErrorSpan>{errorsSignin.email.message} </ErrorSpan>}
                     <Input type="password" placeholder="Senha" name="password" register={registerSignin} />
+                    {errorsSignin.password && <ErrorSpan>{errorsSignin.password.message}</ErrorSpan> }
                     <Button type="submit" text="Entrar" />
                 </form>
             </Section>
 
+
+            
             <Section type="signup">
                 <h2>Cadastrar</h2>
                 <form onSubmit={handleSubmitSignup(upHandleSubmit)}>
                     <Input type="text" placeholder="Nome" name="name" register={registerSignup} />
+                    {errorsSignup.name && <ErrorSpan>{errorsSignup.name.message} </ErrorSpan>}
                     <Input type="email" placeholder="Email" name="email" register={registerSignup} />
+                    {errorsSignup.email && <ErrorSpan>{errorsSignup.email.message} </ErrorSpan>}
                     <Input type="password" placeholder="Senha" name="password" register={registerSignup} />
+                    {errorsSignup.password && <ErrorSpan>{errorsSignup.password.message} </ErrorSpan>}
                     <Input type="password" placeholder="Confirmar senha" name="confirmPassword" register={registerSignup} />
+                    {errorsSignup.confirmPassword && <ErrorSpan>{errorsSignup.confirmPassword.message} </ErrorSpan>}
                     <Button type="submit" text="Cadastrar" />
                 </form>
             </Section>
